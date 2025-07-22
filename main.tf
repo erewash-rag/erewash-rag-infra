3eb7064ad811b88d50d236761cd2721b079b380f
Matt Micklewright <matthewmicklewright@gmail.com>
2025-07-23 00:09:46 +0100

    Article by id endpoint??

--- main.tf
+++ main.tf
@@ -122,9 +122,40 @@ resource "aws_lambda_permission" "apigw_articles" {
   source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/GET/articles"
 }
 
+resource "aws_api_gateway_resource" "article_id" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_resource.articles.id
+  path_part   = "{articleId}"
+}
+
+resource "aws_api_gateway_method" "get_article_id" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.article_id.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "get_article_id" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.article_id.id
+  http_method = aws_api_gateway_method.get_article_id.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.api.invoke_arn
+}
+
+resource "aws_lambda_permission" "apigw_article_id" {
+  statement_id  = "AllowAPIGatewayInvokeArticleId"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.api.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/GET/articles/*"
+}
+
 resource "aws_api_gateway_deployment" "api" {
   depends_on = [
-    aws_api_gateway_integration.get_articles
+    aws_api_gateway_integration.get_articles,
+    aws_api_gateway_integration.get_article_id
   ]
   rest_api_id = aws_api_gateway_rest_api.api.id
 }
