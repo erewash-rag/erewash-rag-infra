7b46a1ce8b4cf50e6d16eb6b127433cfcbda0f99
Matt Micklewright <matthewmicklewright@gmail.com>
2025-07-22 16:40:24 +0100

    Fix syntax error for API gateway stage name

--- main.tf
+++ main.tf
@@ -181,5 +181,10 @@ resource "aws_api_gateway_deployment" "api" {
     aws_api_gateway_integration.get_article_id
   ]
   rest_api_id = aws_api_gateway_rest_api.api.id
-  stage_name  = "prod"
+}
+
+resource "aws_api_gateway_stage" "prod" {
+  stage_name    = "prod"
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  deployment_id = aws_api_gateway_deployment.api.id
 } 
