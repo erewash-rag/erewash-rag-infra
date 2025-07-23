4484a92a14909183b0349c8e3c83d1d1098352f4
Matt Micklewright <matthewmicklewright@gmail.com>
2025-07-23 15:20:33 +0100

    Add POST to API gateway

--- main.tf
+++ main.tf
@@ -203,6 +203,30 @@ resource "aws_lambda_permission" "apigw_article_id" {
   source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/GET/articles/*"
 }
 
+resource "aws_api_gateway_method" "post_articles" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.articles.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "post_articles" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.articles.id
+  http_method             = aws_api_gateway_method.post_articles.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.api.invoke_arn
+}
+
+resource "aws_lambda_permission" "apigw_articles_post" {
+  statement_id  = "AllowAPIGatewayInvokeArticlesPost"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.api.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/POST/articles"
+}
+
 resource "aws_api_gateway_deployment" "api" {
   depends_on = [
     aws_api_gateway_integration.get_articles,
