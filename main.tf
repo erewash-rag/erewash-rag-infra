97d3d7e5ab6f76bde00c02e1c299f290b9322e36
Matt Micklewright <matthewmicklewright@gmail.com>
2025-07-23 17:18:43 +0100

    PUT endpoint

--- main.tf
+++ main.tf
@@ -251,12 +251,37 @@ resource "aws_lambda_permission" "apigw_article_id_delete" {
   source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/DELETE/articles/*"
 }
 
+resource "aws_api_gateway_method" "put_article_id" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.article_id.id
+  http_method   = "PUT"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "put_article_id" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.article_id.id
+  http_method             = aws_api_gateway_method.put_article_id.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.api.invoke_arn
+}
+
+resource "aws_lambda_permission" "apigw_article_id_put" {
+  statement_id  = "AllowAPIGatewayInvokeArticleIdPut"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.api.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/PUT/articles/*"
+}
+
 resource "aws_api_gateway_deployment" "api" {
   depends_on = [
     aws_api_gateway_integration.get_articles,
     aws_api_gateway_integration.post_articles,
     aws_api_gateway_integration.get_article_id,
-    aws_api_gateway_integration.delete_article_id
+    aws_api_gateway_integration.delete_article_id,
+    aws_api_gateway_integration.put_article_id
   ]
   rest_api_id = aws_api_gateway_rest_api.api.id
 }
