5b0b30dd084ccc8f3a87330cbeb460efa9030bc9
Matt Micklewright <matthewmicklewright@gmail.com>
2025-07-23 17:10:51 +0100

    fix

--- main.tf
+++ main.tf
@@ -259,5 +259,10 @@ resource "aws_api_gateway_deployment" "api" {
     aws_api_gateway_integration.delete_article_id
   ]
   rest_api_id = aws_api_gateway_rest_api.api.id
-  stage_name  = "prod"
+}
+
+resource "aws_api_gateway_stage" "prod" {
+  stage_name    = "prod"
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  deployment_id = aws_api_gateway_deployment.api.id
 } 
